355090b439f4620a4658a4b5dba5e2ab53f4dc90
Rex Hsu <weiyao220@gmail.com>
2019-10-27 16:10:11 +0800

    add URI

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, StickerSendMessage, ImagemapSendMessage, ImagemapArea, MessageImagemapAction
+    MessageEvent, TextMessage, TextSendMessage, StickerSendMessage, ImagemapSendMessage, ImagemapArea, MessageImagemapAction, URIImagemapAction
 )
 
 app = Flask(__name__)
