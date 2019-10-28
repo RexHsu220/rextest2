2ee1e2640aecf53936d7a5fff893d07d805bb5b2
Rex Hsu <weiyao220@gmail.com>
2019-10-28 08:39:35 +0800

    deleting sth

--- app.py
+++ app.py
@@ -37,26 +37,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = ImagemapSendMessage(
-        base_url='https://example.com/base',
-        alt_text='this is an imagemap',
-        base_size=BaseSize(height=1040, width=1040),
-        actions=[
-            URIImagemapAction(
-                link_uri='https://example.com/',
-                area=ImagemapArea(
-                    x=0, y=0, width=520, height=1040
-                )
-            ),
-            MessageImagemapAction(
-                text='hello',
-                area=ImagemapArea(
-                    x=520, y=0, width=520, height=1040
-                )
-            )
-        ]
-    )
-    line_bot_api.reply_message(event.reply_token, message)
+    
     msg = event.message.text
     r = '乖小孩'
 
